dc295d9112124eca6629b25d691283d04dc8b7e2
Luís Henrique Domingues Bueno <105614892+LuisHDBueno@users.noreply.github.com>
2024-05-16 17:51:40 -0300

    Create the add data function

--- src/cade_analytics/cade_analytics_simulator.py
+++ src/cade_analytics/cade_analytics_simulator.py
@@ -24,17 +24,31 @@ class CadeAnalyticsSimulator:
             yield data
             time.sleep(speed)
     
+    
+    def add_data(self, queue_data, buffer_output, n = 100, speed = 1):
+        for data in self.time_trigger(n=n, speed=speed):
+            for d in data:
+                queue_data.put(d)
+            while not buffer_output.empty():
+                print(buffer_output.get())
+
+        
+    def run_simulation(self, n = 100, speed = 1):
+        manager = mp.Manager()
+        buffer_output = manager.Queue()
+        reader = rd.Reader(n_threads=4, buffer_output=buffer_output)
+        manager_data = mp.Manager()
+        queue_data = manager_data.Queue()
+        data_process = mp.Process(target=self.add_data, args=(queue_data, buffer_output, n, speed))
+        read_process = mp.Process(target=reader.read_threaded, args=(queue_data,))
+
+        data_process.start()
+        read_process.start()
+        
+        data_process.join()
+        read_process.join()
 
 if __name__ == "__main__":
     cas = CadeAnalyticsSimulator()
-    manager = mp.Manager()
-    buffer_output = manager.Queue()
-    reader = rd.Reader(n_threads=4, buffer_output=buffer_output)
-    manager_data = mp.Manager()
-    queue_data = manager_data.Queue()
-    mp.Process(target=reader.read_threaded, args=(queue_data,)).start()
-    for data in cas.time_trigger(n=5, speed=1):
-        for d in data:
-            queue_data.put(d)
-        while not buffer_output.empty():
-            print(buffer_output.get())
+    cas.run_simulation(n=100, speed=1)
+    
