2e23f920f31adb8b6c65c6976a3d8e2d6e585b6a
Luís Henrique Domingues Bueno <105614892+LuisHDBueno@users.noreply.github.com>
2024-05-16 17:11:21 -0300

    add simulation data

---
+++ src/cade_analytics/cade_analytics_simulator.py
@@ -0,0 +1,40 @@
+import random
+import time
+import multiprocessing as mp
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+import src.framework.reader as rd
+
+class CadeAnalyticsSimulator:
+    def __init__(self, path: str = "data/cade_analytics_data.txt"):
+        self.path = path
+        with open(path, "r") as f:
+            data = f.readlines()
+            data = [line.strip() for line in data]
+        self.data = data
+
+    def get_data(self, n = 100):
+        return [random.choice(self.data) for _ in range(n)]
+    
+    def time_trigger(self, n = 100, speed = 1):
+        while True:
+            data = self.get_data(n)
+            yield data
+            time.sleep(speed)
+    
+
+if __name__ == "__main__":
+    cas = CadeAnalyticsSimulator()
+    manager = mp.Manager()
+    buffer_output = manager.Queue()
+    reader = rd.Reader(n_threads=4, buffer_output=buffer_output)
+    for data in cas.time_trigger(n=5, speed=1):
+        manager_data = mp.Manager()
+        queue_data = manager_data.Queue()
+        for d in data:
+            queue_data.put(d)
+        data_reader = reader.read_threaded(queue_data)
+        while not buffer_output.empty():
+            print(buffer_output.get())
