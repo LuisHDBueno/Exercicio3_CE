42f7884be2044db0c6ae77921bd7b27f694f2858
rckalil <b46837@fgv.edu.br>
2024-05-17 01:44:08 -0300

    Error with host

--- src/cade_analytics/cade_analytics_client.py
+++ src/cade_analytics/cade_analytics_client.py
@@ -1,6 +1,9 @@
 import random
 import time
 import multiprocessing as mp
+import grpc
+import datasender_pb2
+import datasender_pb2_grpc
 
 import os
 import sys
@@ -14,12 +17,35 @@ class CadeAnalyticsClient:
             data = f.readlines()
             data = [line.strip() for line in data]
         self.data = data
+        self.channel = grpc.insecure_channel('localhost:50051', options=(('grpc.enable_http_proxy', 0),))
+        self.stub = datasender_pb2_grpc.DataSenderStub(self.channel)
 
     def get_data(self, n:int = 100):
-        return [random.choice(self.data) for _ in range(n)]
+        str_list = [random.choice(self.data) for _ in range(n)]
+        unified_string = '\n'.join(str_list)
+        return unified_string
     
     def time_trigger(self, n:int = 100, speed:int = 1):
-        while True:
+        for i in range(10):
             data = self.get_data(n)
-            yield data
+            response = self.stub.Sender(datasender_pb2.SendData(data=data))
             time.sleep(speed)
+            print(f"Client {os.getpid()} sent {n} data points with response: {response.check}")
+
+if __name__ == "__main__":
+    if os.environ.get('https_proxy'):
+        del os.environ['https_proxy']
+    if os.environ.get('http_proxy'):
+        del os.environ['http_proxy']
+
+    n_clients = 2
+    client_process = []
+    for i in range(n_clients):
+        client = CadeAnalyticsClient()
+        client_process.append(mp.Process(target=client.time_trigger, args=(100, 1)))
+        client_process[i].start()
+        print(f"Client {i} started")
+    
+    for i in range(n_clients):
+        client_process[i].join()
+        print(f"Client {i} finished")
--- src/cade_analytics/cade_analytics_server.py
+++ src/cade_analytics/cade_analytics_server.py
@@ -8,19 +8,40 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 import src.framework.reader as rd
 import cade_analytics_client as cac
 
+import grpc
+import datasender_pb2
+import datasender_pb2_grpc
+from concurrent import futures
 
 class CadeAnalyticsServer:
+    def __init__(self):
+        self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        datasender_pb2_grpc.add_DataSenderServicer_to_server(self, self.server)
+        self.server.add_insecure_port('[::]:50051')
+        self.is_received = False
 
     def comunication(self):
         # AQUI É O MOCK DO CLIENTE TROCAR PELO CLIENTE REAL
-        client = cac.CadeAnalyticsClient()
-        yield client.get_data()
+        self.server.start()
+        self.server.wait_for_termination()
+
+    def Sender(self, request, context):
+        print(f"Received data")
+        self.received_data = request.data
+        self.is_received = True
+        return datasender_pb2.ConfirmData(check=1)
 
     def add_data(self, queue_data):
+        self.comunication()
         while True:
-            data = self.comunication()
-            for d in data:
-                queue_data.put(d)
+            if self.is_received:
+                data = self.received_data
+                for d in data:
+                    queue_data.put(d)
+                self.received = False
+            else:
+                time.sleep(0.5)
+            
 
     def run_simulation(self):
         manager = mp.Manager()
