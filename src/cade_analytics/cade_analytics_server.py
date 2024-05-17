db00afaf546b6d962db337e9db9470dd0406ee9e
Luís Henrique Domingues Bueno <105614892+LuisHDBueno@users.noreply.github.com>
2024-05-16 21:56:57 -0300

    Add server

--- src/cade_analytics/cade_analytics_server.py
+++ src/cade_analytics/cade_analytics_server.py
@@ -8,26 +8,28 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 import src.framework.reader as rd
 import cade_analytics_client as cac
 
-class CadeAnalyticsServer:
-    def time_trigger(self, n = 100, speed = 1):
-        while True:
-            data = self.get_data(n)
-            yield data
-            time.sleep(speed)
-    
-    def add_data(self, queue_data, data):
-        for d in data:
-            queue_data.put(d)
 
-    def run_simulation(self, n = 100, speed = 1):
+class CadeAnalyticsServer:
+
+    def comunication(self):
+        # AQUI É O MOCK DO CLIENTE TROCAR PELO CLIENTE REAL
+        client = cac.CadeAnalyticsClient()
+        yield client.get_data()
+
+    def add_data(self, queue_data):
+        while True:
+            data = self.comunication()
+            for d in data:
+                queue_data.put(d)
+
+    def run_simulation(self):
         manager = mp.Manager()
         buffer_output = manager.Queue()
         reader = rd.Reader(n_threads=4, buffer_output=buffer_output)
         manager_data = mp.Manager()
         queue_data = manager_data.Queue()
-        client = cac.CadeAnalyticsClient()
-        data_process = mp.Process(target=self.add_data, args=(queue_data, client.time_trigger(n, speed)))
-        read_process = mp.Process(target=reader.read_threaded, args=(queue_data, buffer_output))
+        data_process = mp.Process(target=self.add_data, args=(queue_data, ))
+        read_process = mp.Process(target=reader.read_threaded, args=(queue_data, ))
 
         data_process.start()
         read_process.start()
@@ -37,5 +39,4 @@ class CadeAnalyticsServer:
 
 if __name__ == "__main__":
     cas = CadeAnalyticsServer()
-    cas.run_simulation(n=100, speed=1)
-    
+    cas.run_simulation()
