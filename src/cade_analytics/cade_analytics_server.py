6e15d4c785a37e951f3ef424c21ea7b45de27fa5
rckalil <b46837@fgv.edu.br>
2024-05-17 12:11:44 -0300

    Working on server

--- src/cade_analytics/cade_analytics_server.py
+++ src/cade_analytics/cade_analytics_server.py
@@ -19,30 +19,14 @@ class CadeAnalyticsServer:
         datasender_pb2_grpc.add_DataSenderServicer_to_server(self, self.server)
         self.server.add_insecure_port('[::]:50051')
         self.is_received = False
-
-    def comunication(self):
-        # AQUI É O MOCK DO CLIENTE TROCAR PELO CLIENTE REAL
-        self.server.start()
-        self.server.wait_for_termination()
+        self.data = None
 
     def Sender(self, request, context):
         print(f"Received data")
-        self.received_data = request.data
+        received_data = request.data
         self.is_received = True
-        self.queue_data.put(self.received_data)
+        self.queue_data.put(received_data)
         return datasender_pb2.ConfirmData(check=1)
-
-    def add_data(self):
-        self.comunication()
-        """
-        while True:
-            if self.is_received:
-                data = self.received_data
-                for d in data:
-                    self.queue_data.put(d)
-                self.received = False
-            else:
-                time.sleep(0.5)"""
             
 
     def run_simulation(self):
@@ -51,13 +35,9 @@ class CadeAnalyticsServer:
         reader = rd.Reader(n_threads=4, buffer_output=buffer_output)
         manager_data = mp.Manager()
         self.queue_data = manager_data.Queue()
-        data_process = mp.Process(target=self.add_data, args=())
         read_process = mp.Process(target=reader.read_threaded, args=(self.queue_data, ))
 
-        data_process.start()
         read_process.start()
-        
-        data_process.join()
         read_process.join()
 
 if __name__ == "__main__":
